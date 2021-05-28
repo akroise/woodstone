94dc15a18823f0cf2e36440dec4050e5c0e5f29d
vsvai <vsvaibhavsingh5@gmail.com>
2021-05-28 22:47:49 +0530

    check

--- src/js/hover.js
+++ src/js/hover.js
@@ -7,31 +7,29 @@ function loop_catalog() {
         const pic = document.querySelector("#pic");
         var pic1 = null;
         let i = 1;
-        for (let i = 1; i <= 4; i++) {
+        for (let i = 1; i <= 3; i++) {
 
-            setTimeout(() => {
+            animate = setTimeout(() => {
 
                 console.log("#pic" + i)
                 pic1 = document.querySelector("#pic" + i);
                 pic1.style.display = 'block';
-                if (i === 1) {
+                if (i == 1) {
                     pic.style.display = 'none';
                 }
                 if (i >= 2) {
                     let j = i - 1
                     var pic2 = document.querySelector("#pic" + j);
-                    console.log("#pic" + j)
                     pic2.style.display = 'none';
-                    if (i === 3) {
-                        i = 1;
-                    }
+
                 }
 
             }, i * 2000);
-
+            reset_catalog();
 
         }
     }, 1000);
+    reset_catalog();
 }
 
 function reset_catalog() {
@@ -45,6 +43,7 @@ function reset_catalog() {
     pic3.style.display = 'none';
 
 }
+
 hover.addEventListener("mouseover", event => {
     console.log("Mouse in");
     loop_catalog();
@@ -52,5 +51,6 @@ hover.addEventListener("mouseover", event => {
 
 hover.addEventListener("mouseout", event => {
     console.log("Mouse out");
+    clearTimeout(animate);
     reset_catalog();
 });
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const App = () =>
+    <
+    h1 className = "address" > Hello! < /h1>;
+
+ReactDom.render( < App / > , document.getElementById("app"));
