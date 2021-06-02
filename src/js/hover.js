7ec3d4c7c1f53502c27c040fef8c79f8dd5462e3
Vaibhav Singh <46848906+VSVAI@users.noreply.github.com>
2021-06-02 22:30:05 +0530

    react

--- src/js/hover.js
+++ src/js/hover.js
@@ -1,48 +1,52 @@
-const hover = document.querySelector('#catalog');
+/*
+ * Const hover = document.querySelector('#catalog'),
+ *   resetcatalog = () => {
+ *     const pic = document.querySelector('#pic');
+ *     pic.style.display = 'block';
+ *     const pic1 = document.querySelector('#pic1');
+ *     pic1.style.display = 'none';
+ *     const pic2 = document.querySelector('#pic2');
+ *     pic2.style.display = 'none';
+ *     const pic3 = document.querySelector('#pic3');
+ *     pic3.style.display = 'none';
+ *   };
+ * hover.addEventListener('mouseover', (event) => {
+ *   console.log('Mouse in');
+ *   loopcatalog();
+ * });
+ */
 
-function loop_catalog() {
-  setTimeout(() => {
-    const pic = document.querySelector('#pic');
-    let pic1 = null;
-    const i = 1;
-    for (let i = 1; i <= 3; i++) {
-      animate = setTimeout(() => {
-        console.log(`#pic${i}`);
-        pic1 = document.querySelector(`#pic${i}`);
-        pic1.style.display = 'block';
-        if (i == 1) {
-          pic.style.display = 'none';
-        }
-        if (i >= 2) {
-          const j = i - 1,
-            pic2 = document.querySelector(`#pic${j}`);
-          pic2.style.display = 'none';
-        }
-      }, i * 2000);
-      reset_catalog();
-    }
-  }, 1000);
-  reset_catalog();
-}
+/*
+ * Hover.addEventListener('mouseout', (event) => {
+ *   console.log('Mouse out');
+ *   clearTimeout(animate);
+ *   resetcatalog();
+ * });
+ */
 
-function reset_catalog() {
-  const pic = document.querySelector('#pic');
-  pic.style.display = 'block';
-  const pic1 = document.querySelector('#pic1');
-  pic1.style.display = 'none';
-  const pic2 = document.querySelector('#pic2');
-  pic2.style.display = 'none';
-  const pic3 = document.querySelector('#pic3');
-  pic3.style.display = 'none';
-}
-
-hover.addEventListener('mouseover', (event) => {
-  console.log('Mouse in');
-  loop_catalog();
-});
-
-hover.addEventListener('mouseout', (event) => {
-  console.log('Mouse out');
-  clearTimeout(animate);
-  reset_catalog();
-});
+/*
+ * Const loopcatalog = () => {
+ *   setTimeout(() => {
+ *     const pic = document.querySelector('#pic');
+ *     let i = 1,
+ *       pic1 = null;
+ *     for (let i = 1; i <= 3; i++) {
+ *       setTimeout(() => {
+ *         console.log(`#pic${i}`);
+ *         pic1 = document.querySelector(`#pic${i}`);
+ *         pic1.style.display = 'block';
+ *         if (i == 1) {
+ *           pic.style.display = 'none';
+ *         }
+ *         if (i >= 2) {
+ *           const j = i - 1,
+ *             pic2 = document.querySelector(`#pic${j}`);
+ *           pic2.style.display = 'none';
+ *         }
+ *       }, i * 2000);
+ *       reset_catalog();
+ *     }
+ *   }, 1000);
+ *   resetcatalog();
+ * };
+ */
--- src/js/main.js
+++ src/js/main.js
@@ -5,16 +5,4 @@ import './mobile';
 
 console.log('main.js running');
 
-const btn = document.getElementById('test');
-console.log(btn);
-if (btn) {
-  btn.addEventListener('click', visitingcard());
-}
-const open = 0;
-
-function visitingcard() {
-  console.log('working card');
-  const visit = document.getElementById('visit');
-  visit.innerHTML('<img src="./assests/home1.jpeg"/>');
-}
 // Send button
